88f815fdd9d8793534d98ee51093fc58900b65f7
chaecircle <scw4799@gmail.com>
2022-05-09 14:42:19 +0900

    Modify files

--- study01_test01.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-# 기사 url 
-url = 'https://n.news.naver.com/mnews/article/119/0002601986?sid=101'
-
---
+++ test01.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# 기사 url 
+url = 'https://n.news.naver.com/mnews/article/119/0002601986?sid=101'
+
+st.write(f'''
+         [카카오그룹株 반토막 난 주가...중복상장 논란까지]({url})
+         ''')
+
+
